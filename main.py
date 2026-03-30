443d2e665352254b66bcafcb9e3eef02b980df9b
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-30 17:36:08 -0300

    ruff added and files formatted

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-from datetime import UTC,datetime
+from datetime import UTC, datetime
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 
 app = FastAPI()
 
 fake_db = [
-    {'title':"Criando aplicação com Django",'date':datetime.now(UTC), 'published': True},
-    {'title':"Internacionalizando uma app FastApi",'date':datetime.now(UTC), 'published': True},
-    {'title':"Criando aplicação com Flask",'date':datetime.now(UTC), 'published': False},
-    {'title':"Internacionalizando uma app Starlett",'date':datetime.now(UTC), 'published': True},
+    {
+        "title": "Criando aplicação com Django",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Internacionalizando uma app FastApi",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Criando aplicação com Flask",
+        "date": datetime.now(UTC),
+        "published": False,
+    },
+    {
+        "title": "Internacionalizando uma app Starlett",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
 ]
+
 
 class Post(BaseModel):
     title: str
     date: datetime = datetime.now(UTC)
     published: bool = False
 
-@app.post('/post/', status_code=status.HTTP_201_CREATED)
+
+@app.post("/post/", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     fake_db.append(post.model_dump())
     return post
+
 
 @app.get("/posts/")
 def read_posts(published: bool, limit: int, skip: int = 0):
@@ -30,11 +49,16 @@ def read_posts(published: bool, limit: int, skip: int = 0):
         if post["published"] is published:
             posts.append(post)
     return posts
-    
+
+
 @app.get("/posts/{framework}")
 def read_framework_posts(framework: str):
-    return {'posts':[
-    {'title':f"Criando aplicação com {framework}",'date':datetime.now(UTC)},
-    {'title':f"Internacionalizando uma app {framework}",'date':datetime.now(UTC)},
-]}
-
+    return {
+        "posts": [
+            {"title": f"Criando aplicação com {framework}", "date": datetime.now(UTC)},
+            {
+                "title": f"Internacionalizando uma app {framework}",
+                "date": datetime.now(UTC),
+            },
+        ]
+    }
