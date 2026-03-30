f83b241b5ffb051946c7f59a0067cf49a82a523c
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-30 17:33:20 -0300

    post route created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import UTC,datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,17 +11,30 @@ fake_db = [
     {'title':"Internacionalizando uma app Starlett",'date':datetime.now(UTC), 'published': True},
 ]
 
-@app.get("/posts")
-def read_posts(published: bool = True, skip: int = 0, limit: int = len(fake_db)):
-    return [
-        post
-        for post in fake_db[skip : skip + limit]
-        if post["published"] == published
-    ]
+class Post(BaseModel):
+    title: str
+    date: datetime = datetime.now(UTC)
+    published: bool = False
 
+@app.post('/post/', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    fake_db.append(post.model_dump())
+    return post
+
+@app.get("/posts/")
+def read_posts(published: bool, limit: int, skip: int = 0):
+    posts = []
+    for post in fake_db:
+        if len(posts) == limit:
+            break
+        if post["published"] is published:
+            posts.append(post)
+    return posts
+    
 @app.get("/posts/{framework}")
 def read_framework_posts(framework: str):
     return {'posts':[
     {'title':f"Criando aplicação com {framework}",'date':datetime.now(UTC)},
     {'title':f"Internacionalizando uma app {framework}",'date':datetime.now(UTC)},
 ]}
+
